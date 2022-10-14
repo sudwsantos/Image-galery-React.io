fd8d21a9ddcc39ec5fabad53af1600d31edfb0d7
sudwsantos <sudwsantos2@gmail.com>
2022-10-13 23:06:40 -0300

    Desafio finalizado pero faltan estilos

--- src/App.js
+++ src/App.js
@@ -3,12 +3,43 @@ import React from "react";
 import Header from "./components/Header";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div>
       <Header titulo="Galeria de imágenes con React"/>
-      <Card titulo="Hello"/>
+      <Container>
+        <Row>
+          <Col>
+              <Card imagen="https://placedog.net/500" titulo="Prueba descripcion"/>
+          </Col>
+          <Col>
+              <Card imagen="https://placedog.net/501" titulo="Prueba descripcion"/>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+              <Card imagen="https://placedog.net/502" titulo="Prueba descripcion"/>
+          </Col>
+          <Col>
+              <Card imagen="https://placedog.net/503" titulo="Prueba descripcion"/>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+              <Card imagen="https://placedog.net/504" titulo="Prueba descripcion"/>
+          </Col>
+          <Col>
+              <Card imagen="https://placedog.net/505" titulo="Prueba descripcion"/>
+          </Col>
+        </Row>
+
+      </Container>
       <Footer />
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import  Badge from 'react-bootstrap/Badge';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Badge from 'react-bootstrap/Badge';
 
 const Footer = () => {
   return (
     <div>
         <h3>
-            Galería de imágenes <Badge bg="secondary">2022</Badge>
+            Galería de imágenes. Todos los derechos reservados <Badge bg="secondary">2022</Badge>
         </h3>
     </div>
   )
